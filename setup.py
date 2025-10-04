fd32f4f3c58af8e0a412f52ac32163f296a3a64c
Anjaas85 <anja.stanic2001@gmail.com>
2025-10-04 10:51:40 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nlp_model_merging',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "torch>=2.0",
+        "transformers>=4.40",
+        "datasets",
+        "numpy",
+        "tqdm",
+        "pyyaml",
+    ],
+    entry_points={
+        'console_scripts': [
+            'nlpmerge=src.main:main',
+        ],
+    },
+)
